917888fd6ccb067429f85c523eac598872cf76d3
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-11-20 19:07:34 +0530

    Update SearchRoute UI

--- src/SearchRoute.svelte
+++ src/SearchRoute.svelte
@@ -1,9 +1,9 @@
 <script>
     import { onMount } from "svelte";
     import { fly, fade } from "svelte/transition";
-    import { Link } from "svelte-navigator";
+    import { Link, navigate } from "svelte-navigator";
     import Loader from "./Loader.svelte";
-
+    import { MDCTextField } from "@material/textfield";
     let label = (duration) => {
         return `${Math.round(duration / 60)}:${duration % 60 < 10 ? "0" : ""}${
             duration % 60
@@ -12,7 +12,37 @@
 
     let result;
     onMount(async () => {
+        var search = new MDCTextField(document.querySelector("#search-field"));
+        search.input.addEventListener("focusin", async (event) => {
+            document.getElementById("search-icon").style.color =
+                "var(--accent-color)";
+        });
+        search.input.addEventListener("focusout", async (event) => {
+            document.getElementById("search-icon").style.color = "#aaaaaa";
+        });
+        search.input.addEventListener("keydown", async (event) => {
+            if (event.code === "Enter") {
+                result = undefined;
+                if (search.value === "") {
+                    result = "Lookup for the music of your choice";
+                    console.log(typeof result);
+                } else {
+                    let response = await fetch(
+                        `http://x1yb80pwsn4.herokuapp.com/search?keyword=${search.value}&mode=track`
+                    );
+                    result = await response.json();
+                    result = result["result"];
+                    navigate(`/search/${search.value}`, { replace: true });
+                    search.focus();
+                }
+            }
+        });
+        if (location.pathname.split("/").length < 3) {
+            result = "Lookup for the music of your choice";
+            return;
+        }
         let keyword = location.href.split("/").at(-1);
+        search.value = decodeURIComponent(keyword);
         let response = await fetch(
             `http://x1yb80pwsn4.herokuapp.com/search?keyword=${decodeURIComponent(
                 keyword
@@ -20,53 +50,119 @@
         );
         result = await response.json();
         result = result["result"];
-        console.log(result);
+        search.focus();
     });
 </script>
 
-{#if result == undefined}
-    <div class="loader-container">
-        <Loader />
-    </div>
-{:else}
-    <div class="scaffold">
-        {#each result as track}
-            <Link
-                style="text-decoration: none;
+<div class="search-container">
+    <i
+        id="search-icon"
+        style="font-size:32px; color: #aaaaaa; margin-right: 12px;"
+        class="material-icons">search</i
+    >
+    <label id="search-field" class="mdc-text-field mdc-text-field--outlined">
+        <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading" />
+            <span class="mdc-notched-outline__notch">
+                <span
+                    style="font-family: var(--font-family); font-size: 15px"
+                    class="mdc-floating-label"
+                    id="my-label-id">Lookup Music</span
+                >
+            </span>
+            <span class="mdc-notched-outline__trailing" />
+        </span>
+        <input
+            style="font-family: var(--font-family); font-size: 15px"
+            type="text"
+            class="mdc-text-field__input"
+            aria-labelledby="my-label-id"
+        />
+    </label>
+</div>
+<div class="scrollview">
+    {#if result === undefined}
+        <div class="loader-container">
+            <Loader />
+        </div>
+    {:else if typeof result === "string"}
+        <i
+            id="search-icon"
+            style="font-size:280px; z-index: -1000; color: #cccccc; margin-right: 12px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+            class="material-icons">music_note</i
+        >
+        <div class="centered-label">
+            <div>{result}</div>
+        </div>
+    {:else}
+        <div class="scaffold">
+            {#each result as track}
+                <Link
+                    style="text-decoration: none;
             margin-top: 12px;
             margin-bottom: 12px;
             margin-left: 24px;
             margin-right: 24px;"
-                to={`/track/${track["track_id"]}`}
-            >
-                <div
-                    in:fly={{ delay: 100, duration: 500, y: 100 }}
-                    out:fade={{ duration: 500 }}
-                    class="track-container"
+                    to={`/track/${track["track_id"]}+${track["album_id"]}`}
                 >
-                    <img
-                        class="album-art"
-                        src={track["album_art_low"]}
-                        alt="album_art"
-                    />
-                    <div>
+                    <div
+                        in:fly={{ delay: 100, duration: 500, y: 100 }}
+                        out:fade={{ duration: 500 }}
+                        class="track-container mdc-ripple-surface"
+                    >
+                        <img
+                            class="album-art"
+                            src={track["album_art_low"]}
+                            alt="album_art"
+                        />
                         <div>
-                            {track["track_name"]}
-                        </div>
-                        <div class="subtitle">
-                            {track["album_name"]}
-                        </div>
-                        <div class="subtitle">
-                            {label(track["track_duration"])}
+                            <div>
+                                {track["track_name"]}
+                            </div>
+                            <div class="subtitle">
+                                {track["album_name"]}
+                            </div>
+                            <div class="subtitle">
+                                {label(track["track_duration"])}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Link>
-        {/each}
-    </div>
-{/if}
+                </Link>
+            {/each}
+        </div>
+    {/if}
+</div>
 
 <style>
+    .mdc-text-field {
+        width: 100%;
+        margin-right: 12px;
+    }
+
+    .centered-label {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .search-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-top: 18px;
+        padding-bottom: 16px;
+        padding-left: 18px;
+        padding-right: 10px;
+        box-sizing: border-box;
+        position: absolute;
+        z-index: 1000;
+        background-color: var(--card-color);
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    }
+
     .scaffold {
         display: flex;
         flex-direction: column;
@@ -76,6 +172,16 @@
         padding-top: 12px;
         padding-bottom: 12px;
         box-sizing: border-box;
+        overflow-y: scroll;
+        height: 100vh;
+        padding-top: 104px;
+    }
+
+    .scrollview {
+        overflow: hidden;
+        height: 100%;
+        width: 100%;
+        flex: 1;
     }
 
     .loader-container {
