1d4fb5738cb649a53ff6b34525081b04cd9c2252
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2021-11-20 19:31:12 +0530

    Improve routing structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,16 @@
 <script>
-  import TrackRoute from "./TrackRoute.svelte";
-  import AlbumRoute from "./AlbumRoute.svelte";
-  import SearchRoute from "./SearchRoute.svelte";
-  import { Router, Route, Link } from "svelte-navigator";
+  import TrackRoute from "./Track.svelte";
+  import Home from "./Home.svelte";
+  import { Router, Route } from "svelte-navigator";
 
-  fetch("http://x1yb80pwsn4.herokuapp.com");
+  fetch("https://x1yb80pwsn4.herokuapp.com");
 </script>
 
 <Router>
   <Route path="track/*">
     <TrackRoute />
   </Route>
-  <Route path="album/*">
-    <AlbumRoute />
-  </Route>
-  <Route path="search/*">
-    <SearchRoute />
+  <Route path="/*">
+    <Home />
   </Route>
 </Router>
